5264d6b173344013dc07fb1aff528febd02d381c
Jacob Cox <cox@mwsc.vic.edu.au>
2024-02-16 10:38:30 +1100

    .

---
+++ app.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+//import { invoke } from "@tauri-apps/api/tauri";
+
+// call rust function
+// const showHello = async () => {
+//   hello.value == "hello_test"
+//     ? (hello.value = "Click for hello")
+//     : (hello.value = (await invoke("hello_world")) as string);
+// };
+</script>
+
+<template>
+  <div class="bg-green-200 h-[200vh]">
+    <div class="flex justify-center mx-52 rounded-lg">
+      <div class="flex flex-col mt-10 w-full rounded-lg">test</div>
+    </div>
+  </div>
+</template>
---
+++ components/Loader.vue
@@ -0,0 +1,26 @@
+<template>
+  <!-- loader -->
+  <div class="loader absolute m-auto left-0 right-0 z-40"></div>
+</template>
+
+<style scoped>
+/* HTML: <div class="loader"></div> */
+.loader {
+  width: 150px;
+  padding: 8px;
+  aspect-ratio: 1;
+  border-radius: 50%;
+  background: #4fbcff;
+  --_m: conic-gradient(#0000 10%, #000), linear-gradient(#000 0 0) content-box;
+  -webkit-mask: var(--_m);
+  mask: var(--_m);
+  -webkit-mask-composite: source-out;
+  mask-composite: subtract;
+  animation: l3 1s infinite linear;
+}
+@keyframes l3 {
+  to {
+    transform: rotate(1turn);
+  }
+}
+</style>
