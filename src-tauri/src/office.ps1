0d39d336b4fc25df714fc34bd6f04954c78bc982
jacobjmc <111402762+jacobjmc@users.noreply.github.com>
2024-03-01 15:55:13 +1100

    .

--- src-tauri/src/office.ps1
+++ src-tauri/src/office.ps1
@@ -1,4 +1,13 @@
+# Start-BitsTransfer "https://aka.ms/getwinget" -Destination "$env:TEMP\wingetInstaller.msixbundle"
+# Get-BitsTransfer
+# Add-AppxPackage -Path "$env:TEMP\wingetInstaller.msixbundle"
+
+# Start-Process winget.exe -ArgumentList "install --id=Microsoft.Office -e -h --accept-source-agreements --accept-package-agreements" -WindowStyle Hidden
+# Start-Sleep -Seconds 3
+# Get-Process * | Set-WindowState -State Minimize -ErrorAction SilentlyContinue
+
 Start-BitsTransfer "https://byod.mwsc.vic.edu.au/applications/22/download" -Destination "$env:TEMP\OfficeSetup.exe"
 Get-BitsTransfer
 Start-Process -FilePath "$env:TEMP\OfficeSetup.exe"
+
 
